02f145f771315f3ccd160943e9c0fe624ccfbf83
Ritesh <ritesh.wd@suffescom.com>
2024-12-31 11:58:35 +0530

    backup

--- src/Assets/styles/thresholdStyled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const CardCstm = styled.div`
-  background-color: var(--cardBg);
-  border: 1px solid #737373;
-  font-size: 14px;
-  line-height: 20px;
-`;
--- src/lib/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-/* Instruments */
-import {
-  authSlice,
-} from "./slices";
-
-export const reducer = {
-  Auth: authSlice.reducer,
-};
