260dec2c2d719f7e06dcbd278eaf7bc4837e6e71
parvinderkumarSuffescom <parvinder.kumar@suffescom.com>
2025-02-13 21:39:22 +0530

    Added changes

--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -430,7 +430,7 @@ const Setting: React.FC = () => {
                           Subdomain Name:
                         </div>
                         <span className="text-white flex items-center">
-                          quincy120{" "}
+                          --
                         </span>
                       </li>
                     </ul>
