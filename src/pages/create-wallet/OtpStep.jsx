752663227a6eb34d7de533d04712c5b7ac11aa6c
parvinderkumarSuffescom <parvinder.kumar@suffescom.com>
2025-02-21 18:25:50 +0530

    Added changes

--- src/pages/create-wallet/OtpStep.jsx
+++ src/pages/create-wallet/OtpStep.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import OtpInput from "react-otp-input";
 import Image from "next/image";
 import styled from "styled-components";
