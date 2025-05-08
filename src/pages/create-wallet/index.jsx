779231c28e41309b40a49a52fe69837272eba227
parvinderkumarSuffescom <parvinder.kumar@suffescom.com>
2025-05-08 21:48:04 +0530

    Added changes

--- src/pages/create-wallet/index.jsx
+++ src/pages/create-wallet/index.jsx
@@ -421,12 +421,12 @@ const CreateWallet = () => {
         let OTP = generateOTP(4);
         setCheckOTP(OTP);
         setOtpTimestamp(new Date().getTime()); // Save the timestamp when OTP is generated
-        console.log("OTP-->", OTP);
+        // console.log("OTP-->", OTP);
         setRegisterData({
           email: data.email,
           username: data.username,
         });
-        return true;
+        // return true;
         let obj = {
           email: data.email,
           name: data.username,
