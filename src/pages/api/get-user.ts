26188925e58cc0e363f03137485840dded1a91c3
parvinderkumarSuffescom <parvinder.kumar@suffescom.com>
2025-05-09 18:14:17 +0530

    Added

--- src/pages/api/get-user.ts
+++ src/pages/api/get-user.ts
@@ -5,7 +5,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
-
+ 
     try {
         const { email, type = "", webAuthKey = "", ensName = "" } = req.body;
         // Connect to the database
