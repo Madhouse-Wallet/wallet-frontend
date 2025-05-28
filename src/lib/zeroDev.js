f6a3fdd694d49be19550a6f6d2122a422833545a
Quincy Walker <qwalker@nvidia.com>
2025-05-28 15:19:25 -0400

    clean error with sign up

--- src/lib/zeroDev.js
+++ src/lib/zeroDev.js
@@ -145,7 +145,7 @@ export const setupNewAccount = async (
 
     const hash = await relayClient.sendTransaction({
       to: walletAddress,
-      value: parseEther("0.000001"),
+      value: parseEther("0.00001"),
     });
 
     console.log(
