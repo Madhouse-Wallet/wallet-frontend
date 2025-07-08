ce45ea5e4d9f3cdfdf2580f5786e5d5969fbd3bd
parvinderkumarSuffescom <parvinder.kumar@suffescom.com>
2025-07-08 19:08:13 +0530

    Added changes for bitcoin

--- src/components/Modals/WithdrawPop/index.jsx
+++ src/components/Modals/WithdrawPop/index.jsx
@@ -120,6 +120,7 @@ const WithdrawPopup = ({ withdrawPop, setWithdrawPop }) => {
           } else {
             fetchLighteningBalance();
             setLoading(false);
+            setSuccess(true)
           }
         }
       }
