2462c18714cd6673dec39c344bb6bfcf4c1cb405
HerambSharanSuffescom <heramb@suffescom.co>
2025-07-17 12:03:22 +0530

    add boltz automatic trxn

--- src/pages/btc-exchange/RecentTransaction.jsx
+++ src/pages/btc-exchange/RecentTransaction.jsx
@@ -107,6 +107,8 @@ const RecentTransaction = ({ setSetFilterType }) => {
       fetchTPOSWithdrawTransactions(8);
     } else if (activeTab === 10) {
       fetchTPOSWithdrawTransactions(10);
+    } else if (activeTab === 13) {
+      fetchTPOSWithdrawTransactions(13);
     }
   };
 
@@ -424,7 +426,7 @@ const RecentTransaction = ({ setSetFilterType }) => {
           },
           "madhouse-backend-production-getUserTrxn"
         );
-      } else {
+      } else if (key === 10) {
         sideShiftIdsData = await lambdaInvokeFunction(
           {
             email: userAuth?.email,
@@ -432,6 +434,17 @@ const RecentTransaction = ({ setSetFilterType }) => {
             page: 1,
             limit: 100,
             subType: "tpos",
+          },
+          "madhouse-backend-production-getUserTrxn"
+        );
+      } else {
+        sideShiftIdsData = await lambdaInvokeFunction(
+          {
+            email: userAuth?.email,
+            type: "boltz",
+            page: 1,
+            limit: 100,
+            subType: "automatic",
           },
           "madhouse-backend-production-getUserTrxn"
         );
@@ -1205,6 +1218,91 @@ const RecentTransaction = ({ setSetFilterType }) => {
         </>
       ),
     },
+    {
+      title: "Boltz Lightning Automatic Withdraw",
+      component: (
+        <>
+          {withdrawBoltz.length > 0 ? (
+            <div className="bg-black/5 lg:p-4 rounded-lg p-3 ">
+              {Object.entries(transactionsByDateTPOSWithdraw).map(
+                ([date, txs]) => {
+                  return (
+                    <div key={date} className="py-3">
+                      <p className="m-0 text-white text-xs font-semibold pb-2">
+                        {date}
+                      </p>
+                      <div className="grid gap-3 grid-cols-12">
+                        {txs
+                          .filter((tx) => {
+                            const amount = parseFloat(
+                              tx.amount?.split(" ")[0] || 0
+                            );
+                            return amount >= 0.00000001;
+                          })
+                          .map((tx, key) => (
+                            <div
+                              key={key}
+                              className="md:col-span-6 col-span-12"
+                            >
+                              <div
+                                onClick={() => handleTposTransactionClick(tx)}
+                                className="bg-white/5 p-3 rounded-lg flex items-start gap-2 justify-between cursor-pointer hover:bg-black/60"
+                              >
+                                <div className="left flex items-start gap-2">
+                                  <div className="flex-shrink-0 h-[40px] w-[40px] rounded-full flex items-center justify-center bg-white/50">
+                                    {sendSvg}
+                                  </div>
+                                  <div className="content">
+                                    <h4 className="m-0 font-bold md:text-base">
+                                      {"Boltz Automatic Withdraw"}{" "}
+                                      {tx.amount?.split(" ")[1] || "BTC"}
+                                    </h4>
+                                    {/* <p
+                                      className={`m-0 ${getStatusColor(
+                                        tx.status
+                                      )} font-medium text-xs`}
+                                    >
+                                      {getStatusText(tx.status)}
+                                    </p> */}
+                                  </div>
+                                </div>
+                                <div className="right text-right">
+                                  <p className="m-0  text-xs font-medium py-1">
+                                    {tx.status === "rejected"
+                                      ? "Insufficient Balance"
+                                      : `${tx?.type === "send" ? "-" : "+"} ${parseFloat(
+                                          tx.amount?.split(" ")[0] || 0
+                                        ).toFixed(8)}`}{" "}
+                                    {/* Changed to 8 decimal places for BTC */}
+                                  </p>
+                                  <p className="m-0 text-xs font-medium py-1">
+                                    {tx.day}
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          ))}
+                      </div>
+                    </div>
+                  );
+                }
+              )}
+            </div>
+          ) : (
+            <>
+              <Image
+                src={process.env.NEXT_PUBLIC_IMAGE_URL + "noData.png"}
+                alt=""
+                height={10000}
+                width={10000}
+                style={{ maxHeight: 400 }}
+                className="max-w-full h-auto w-auto mx-auto"
+              />
+            </>
+          )}
+        </>
+      ),
+    },
   ];
 
   return (
@@ -1312,7 +1410,7 @@ const RecentTransaction = ({ setSetFilterType }) => {
                       </svg>
                     </div>
                   </div>
-                ) : activeTab === 8 || activeTab === 10 ? (
+                ) : activeTab === 8 || activeTab === 10 || activeTab === 13 ? (
                   <></>
                 ) : (
                   <>
@@ -1368,6 +1466,7 @@ const RecentTransaction = ({ setSetFilterType }) => {
                   else if (activeTab === 10) dataToExport = withdrawBoltz;
                   else if (activeTab === 11) dataToExport = boltzBitcoin;
                   else if (activeTab === 12) dataToExport = feeTransaction;
+                  else if (activeTab === 13) dataToExport = withdrawBoltz;
                   if (dataToExport.length) {
                     exportTransactionsToCSV(
                       dataToExport,
@@ -1428,6 +1527,8 @@ const RecentTransaction = ({ setSetFilterType }) => {
                             fetchTPOSWithdrawTransactions(8);
                           } else if (key === 10) {
                             fetchTPOSWithdrawTransactions(10);
+                          } else if (key === 13) {
+                            fetchTPOSWithdrawTransactions(13);
                           }
                         }}
                         className={`block px-4 text-xs py-2 w-full text-left flex items-center justify-between ${
