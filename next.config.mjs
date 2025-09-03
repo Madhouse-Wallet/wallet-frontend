18f53373f69949a158413c78fe0bb7b109c2bd47
WanjikuNjoroge-dev <142795635+WanjikuNjoroge-dev@users.noreply.github.com>
2025-09-03 03:18:37 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,7 @@ const nextConfig = {
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' }
+        ]
       }
     ];
   },
