92edea19ba11b6e82ebc3aba15bb37169a72057f
zack92133-sudo <zack92133@gmail.com>
2025-09-16 00:52:23 +0530

    removed the required

--- src/pages/spherepay/TransferHistory.jsx
+++ src/pages/spherepay/TransferHistory.jsx
@@ -854,7 +854,6 @@ const TransferHistory = ({ step, setStep, customerId }) => {
                     onChange={handleOffRampChange}
                     className="border-white/10 bg-white/4 hover:bg-white/6 focus-visible:placeholder:text-white/40 text-white/40 focus-visible:text-white focus-visible:border-white/50 focus-visible:bg-white/10 placeholder:text-white/30 flex text-xs w-full border-px md:border-hpx px-5 py-2 text-15 font-medium -tracking-1 transition-colors duration-300 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-40 h-12 rounded-full pr-11"
                     placeholder="Enter Message"
-                    required
                   />
                 </div>
 
