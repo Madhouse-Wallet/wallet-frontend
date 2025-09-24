98c5bb8df947098857f2ea2e56457b920c18c045
Quincy Walker <qwalker@nvidia.com>
2025-09-24 18:47:50 -0400

    remove unused code

--- src/pages/api/create-card.ts
+++ src/pages/api/create-card.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { createPass, deletePass } from "./passninjaCard";
+//import { createPass, deletePass } from "./passninjaCard";
 import { lambdaInvokeFunction } from "../../lib/apiCall";
 
 
@@ -18,36 +18,36 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 
         const apiResponse = await lambdaInvokeFunction({ email }, "madhouse-backend-production-getUser") as any;
-        if (apiResponse?.status == "success") {
-            let existingUser = apiResponse?.data;
-            if (existingUser) {
-                if (existingUser?.creditCardPass) {
-                    return res.status(400).json({ status: "failure", error: "Already Created!" });
-                } else {
-                    let creditCardDetails = await createPass(existingUser?.wallet, type)
-                    if (creditCardDetails?.status) {
-                        const existingUser = await lambdaInvokeFunction({
-                            findData: { email }, updtData: {
-                                $set: {
-                                    creditCardPass: creditCardDetails.data
-                                }
-                            }
-                        }, "madhouse-backend-production-updtUser") as any;
-                        if (existingUser?.status == "success") {
-                            return res.status(200).json({ status: "success", message: 'Card Created successfully!', data: existingUser?.data });
-                        } else {
-                            return res.status(400).json({ status: "failure", message: existingUser?.message, error: existingUser?.error });
-                        }
-                    } else {
-                        return res.status(400).json({ status: "failure", error: creditCardDetails?.msg });
-                    }
-                }
-            } else {
-                return res.status(400).json({ status: "failure", error: "User Does Not Exist!" });
-            }
-        } else {
-            return res.status(400).json({ status: "failure", message: apiResponse?.message, error: apiResponse?.error });
-        }
+        // if (apiResponse?.status == "success") {
+        //     let existingUser = apiResponse?.data;
+        //     if (existingUser) {
+        //         if (existingUser?.creditCardPass) {
+        //             return res.status(400).json({ status: "failure", error: "Already Created!" });
+        //         } else {
+        //             let creditCardDetails = await createPass(existingUser?.wallet, type)
+        //             if (creditCardDetails?.status) {
+        //                 const existingUser = await lambdaInvokeFunction({
+        //                     findData: { email }, updtData: {
+        //                         $set: {
+        //                             creditCardPass: creditCardDetails.data
+        //                         }
+        //                     }
+        //                 }, "madhouse-backend-production-updtUser") as any;
+        //                 if (existingUser?.status == "success") {
+        //                     return res.status(200).json({ status: "success", message: 'Card Created successfully!', data: existingUser?.data });
+        //                 } else {
+        //                     return res.status(400).json({ status: "failure", message: existingUser?.message, error: existingUser?.error });
+        //                 }
+        //             } else {
+        //                 return res.status(400).json({ status: "failure", error: creditCardDetails?.msg });
+        //             }
+        //         }
+        //     } else {
+        //         return res.status(400).json({ status: "failure", error: "User Does Not Exist!" });
+        //     }
+        // } else {
+        //     return res.status(400).json({ status: "failure", message: apiResponse?.message, error: apiResponse?.error });
+        // }
     } catch (error) {
         console.error('Error adding user:', error);
         return res.status(500).json({ error: 'Internal server error' });
--- src/pages/api/delete-card.ts
+++ src/pages/api/delete-card.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { createPass, deletePass } from "./passninjaCard";
+//import { createPass, deletePass } from "./passninjaCard";
 import { lambdaInvokeFunction } from "../../lib/apiCall";
 
 
@@ -9,41 +9,41 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     try {
-        const { email } = req.body;
-        // Validate email
-        // Validate email
-        if (!email || typeof email !== 'string') {
-            return res.status(400).json({ error: 'Invalid email' });
-        }
+        // const { email } = req.body;
+        // // Validate email
+        // // Validate email
+        // if (!email || typeof email !== 'string') {
+        //     return res.status(400).json({ error: 'Invalid email' });
+        // }
 
-        const apiResponse = await lambdaInvokeFunction({ email }, "madhouse-backend-production-getUser") as any;
+        // const apiResponse = await lambdaInvokeFunction({ email }, "madhouse-backend-production-getUser") as any;
 
-        if (apiResponse?.status == "success") {
-            let existingUser = apiResponse?.data;
-            if (existingUser?.creditCardPass) {
-                let deleteCardResults = await deletePass(existingUser?.creditCardPass?.serialNumber, existingUser?.creditCardPass?.type)
-                if (deleteCardResults?.status) {
-                    const existingUser = await lambdaInvokeFunction({
-                        findData: { email }, updtData: {
-                            $set: {
-                                creditCardPass: ""
-                            }
-                        }
-                    }, "madhouse-backend-production-updtUser") as any;
-                    if (existingUser?.status == "success") {
-                        return res.status(200).json({ status: "success", message: 'Card Deleted successfully!', data: existingUser?.data });
-                    } else {
-                        return res.status(400).json({ status: "failure", message: existingUser?.message, error: existingUser?.error });
-                    }
-                } else {
-                    return res.status(400).json({ status: "failure", error: deleteCardResults?.msg });
-                }
-            } else {
-                return res.status(400).json({ status: "failure", error: "No card Found!" });
-            }
-        } else {
-            return res.status(400).json({ status: "failure", message: apiResponse?.message, error: apiResponse?.error });
-        }
+        // if (apiResponse?.status == "success") {
+        //     let existingUser = apiResponse?.data;
+        //     if (existingUser?.creditCardPass) {
+        //         let deleteCardResults = await deletePass(existingUser?.creditCardPass?.serialNumber, existingUser?.creditCardPass?.type)
+        //         if (deleteCardResults?.status) {
+        //             const existingUser = await lambdaInvokeFunction({
+        //                 findData: { email }, updtData: {
+        //                     $set: {
+        //                         creditCardPass: ""
+        //                     }
+        //                 }
+        //             }, "madhouse-backend-production-updtUser") as any;
+        //             if (existingUser?.status == "success") {
+        //                 return res.status(200).json({ status: "success", message: 'Card Deleted successfully!', data: existingUser?.data });
+        //             } else {
+        //                 return res.status(400).json({ status: "failure", message: existingUser?.message, error: existingUser?.error });
+        //             }
+        //         } else {
+        //             return res.status(400).json({ status: "failure", error: deleteCardResults?.msg });
+        //         }
+        //     } else {
+        //         return res.status(400).json({ status: "failure", error: "No card Found!" });
+        //     }
+        // } else {
+        //     return res.status(400).json({ status: "failure", message: apiResponse?.message, error: apiResponse?.error });
+        // }
     } catch (error) {
         console.error('Error adding user:', error);
         return res.status(500).json({ error: 'Internal server error' });
