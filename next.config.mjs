35ccafe83e951962ed7a1ce0f8bf5b8163427208
Quincy Walker <qwalker@nvidia.com>
2025-09-25 08:09:06 -0400

    added clickjacking protection

--- next.config.mjs
+++ next.config.mjs
@@ -17,11 +17,7 @@ const nextConfig = {
           { key: 'Content-Security-Policy', value: "frame-ancestors 'none'" },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
-          { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
-            {
-          key: 'Content-Security-Policy',
-          value: "frame-ancestors 'none';",
-        },
+          { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' }
         ]
       }
     ];
